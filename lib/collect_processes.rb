e0628e34056c7d14a5d89000f15f79641bc82882
Casey Ellett <casey.ellett@gmail.com>
2012-09-15 20:56:33 -0400

    Some comments.

--- lib/collect_processes.rb
+++ lib/collect_processes.rb
@@ -3,24 +3,32 @@ module GetRunningProcesses
     def self.read_processes
       output = `ps -ef`
 
+      # Container for returned processes
       processes = []
 
+      # Container for processing the lines
       lines = []
 
+      # Grab the lines from ps and collect into Arrays
       output.lines.each do |line|
         process = line.split(' ')
         lines << process
       end
 
+      # Process the lines from ps and concat the command names
       lines.each do |line|
+        # Skip the header row
+        # TODO: Use this header row to extract PID, CMD, etc.
         if line[0] == "UID"
         else
+          # Concatenate the command after splitting on spaces
           result = line.slice!(7, 30).join(' ').strip
           line << result
           processes << line
         end
       end
 
+      # Returns the Array
       return processes
     end
   end
